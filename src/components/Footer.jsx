bf6650c9988ea558c87a77df91dbe18cf3c2c449
august269 <akirui95@gmail.com>
2023-03-22 08:28:06 +0300

    add dynamic year to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Header() {
+function Footer() {
     const thisYear = new Date().getFullYear();
     return <footer className="footer">Copyright {thisYear}</footer>;
 };
 
-export default Header;
+export default Footer;
