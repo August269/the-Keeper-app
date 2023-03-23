d03b35e23a1ba32dba1f9150a9a3ee831e1859db
august269 <akirui95@gmail.com>
2023-03-23 20:17:12 +0300

    add form to add notes, add delete functionality on clicking the note

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+// import Noteslist from "./notes";
+import Form from "./components/form";
+import React, { useState } from "react";
+
 
 function App() {
+  const [list, updateList] = useState([]);
+
+  //function to add items to the dynamic state list
+  function addNote(item) {
+    updateList(prevValues => {
+      return [...prevValues,
+        item
+      ];
+    });
+  };
+
+  function deleteItem(index) {
+    updateList((prevItems) => {
+      let tempList = [...prevItems];//use new list bc prevItems is immutable
+      tempList.splice(index, 1);
+      return tempList;
+    })
+  }
+
   return (
     <div className="App">
       <Header />
+      <section>
+        <Form
+          addNote={addNote}
+        />
+      </section>
+      <section>
+        {list.map((item, index) => <Note
+          key={index}
+          index={index}
+          title={item.title}
+          content={item.note}
+          deleteItem={deleteItem}
+        />
+        )}
+      </section>
       <Footer />
-      <Note />
     </div>
   );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Note() {
-    return <div className="note">
-        <h1>Title</h1>
-        <p>Paragraph</p>
+function Note(props) {
+    return <div className="note" onClick={() => (props.deleteItem(props.index))}>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>;
 };
 
