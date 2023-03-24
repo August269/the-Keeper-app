b364a72d444ea38479a5fd4665a8647373332b69
august269 <akirui95@gmail.com>
2023-03-24 07:51:21 +0300

    add material-ui icons for styling

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Note(props) {
-    return <div className="note" onClick={() => (props.deleteItem(props.index))}>
+    return <div className="note" >
         <h1>{props.title}</h1>
         <p>{props.content}</p>
+        <button onClick={() => (props.deleteItem(props.index))}>
+            <DeleteIcon />
+        </button>
     </div>;
 };
 
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function Form(props) {
     const [form, setForm] = useState({ title: "", note: "" });
+    const [isExpanded, setIsExpanded] = useState(false);
 
     function handleChange(event) {
         const value = event.target.value;
@@ -15,19 +19,28 @@ function Form(props) {
         });
     };
 
+    //to modify the isExpanded state when the input text-area is clicked
+    function handleExpansion() {
+        setIsExpanded(true);
+    }
+
     //to prevent the form from posting when submitted and refreshing the page
     function formSubmitHandler(event) {
         event.preventDefault();
     }
 
-    return <form action="" onSubmit={formSubmitHandler}>
-        <h1>New note</h1>
-        <input value={form.title} name="title" onChange={handleChange} placeholder="Enter title"></input>
-        <textarea value={form.note} name="note" onChange={handleChange} placeholder="Enter note"></textarea>
-        <button onClick={() => {
-            props.addNote(form);
-            setForm({ title: "", note: "" });
-        }} >Add</button>
+    return <form action="" onSubmit={formSubmitHandler} className="create-note" >
+        <input value={form.title} name="title" onChange={handleChange} placeholder="Title" className={!isExpanded && "visually-hidden"}></input>
+        <textarea value={form.note} name="note" onChange={handleChange} placeholder="Create a note..." rows={isExpanded ? 3 : 1} onClick={handleExpansion}></textarea>
+        <Zoom in={isExpanded}>
+            <Fab onClick={() => {
+                props.addNote(form);
+                setForm({ title: "", note: "" });
+                setIsExpanded(false);
+            }} >
+                <AddCircleOutlineIcon />
+            </Fab>
+        </Zoom>
     </form>
 };
 
